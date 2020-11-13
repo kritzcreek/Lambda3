74eb41a8c5e7d6011c838259dea6b78c21c18cd7
David Petersen <david@familiep.de>
2020-11-13 14:21:33 +0100

    defines ast for expressions

---
+++ src/ast.rs
@@ -0,0 +1,29 @@
+use crate::cst;
+use crate::cst::ExprKind;
+use std::rc::Rc;
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub enum Expr {
+    Var(String),
+    Lambda(String, Rc<Expr>),
+    App(Rc<Expr>, Rc<Expr>),
+}
+
+impl Expr {
+    pub fn from_cst(cst: cst::Expr) -> Option<Self> {
+        match cst.kind() {
+            ExprKind::Var(var) => Some(Expr::Var(var.name())),
+            ExprKind::Lambda(lambda,) => {
+                let binder = lambda.binder()?;
+                let body  = lambda.body()?;
+                let body = Self::from_cst(body)?;
+                Some(Expr::Lambda(binder, Rc::new(body)))
+            }
+            ExprKind::Application(app) => {
+                let func = Self::from_cst(app.func())?;
+                let arg = Self::from_cst(app.arg())?;
+                Some(Expr::App(Rc::new(func), Rc::new(arg)))
+            }
+        }
+    }
+}
